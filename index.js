3b07333234617fc4ac8b35ddf4f0c16551931fc2
Serhii <sergey.petrunya@gmail.com>
2018-02-24 20:06:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ var updateObjectWithKeyAndValue = (object, key, value) => {
   return Object.assign({}, object, { [key]: value })
 }
 
+var deleteFromObjectByKey = (object, key) => {
+  var objc = Object.assign({}, object, [key])
+  delete objc[key];
+  return objc;
+}
 
 var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
   object[key] = value;
   return object;
 }
 
-var deleteFromObjectByKey = (object, key) => {
-  var objc = Object.assign({}, object, [key])
-  delete objc[key];
-  return objc;
+var destructivelyDeleteFromObjectByKey = (object, key) => {
+  this.object = object
+  this.key = key
+  delete object.key
 }
+
